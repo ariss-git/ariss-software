8b68f38bcc69b0d126ebc6d429e90b577279fafe
Saad Sayyed <saadsyed950@gmail.com>
2025-10-06 17:25:42 +0530

    Panel app setup

---
+++ panel-app/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen">
+      <h1 className="text-8xl">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
