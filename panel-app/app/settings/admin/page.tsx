995bc9cf5d7ea7c28492191875151e83a351ec77
Saad Sayyed <saadsyed950@gmail.com>
2025-10-06 20:40:13 +0530

    Admins table layout

--- panel-app/app/settings/admin/page.tsx
+++ panel-app/app/settings/admin/page.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-import apiURL from "@/app/_api/api-url";
 import { fetchAllAdmins } from "@/app/_api/panel-user";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useEffect, useState } from "react";
 
 interface Admin {
@@ -15,6 +29,20 @@ interface Admin {
 
 const FetchAllAdmins = () => {
   const [adminData, setAdminData] = useState<Admin[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const filteredData = adminData.filter((admin) =>
+    admin.fullname.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const paginatedData = filteredData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
 
   const getAllAdmins = async () => {
     try {
@@ -31,18 +59,74 @@ const FetchAllAdmins = () => {
   }, []);
 
   return (
-    <div>
-      {adminData.map((item) => (
-        <div
-          className="flex justify-center items-center flex-col mb-4"
-          key={item.panel_user_id}
-        >
-          <h1>{item.fullname}</h1>
-          <h1>{item.email}</h1>
-          <h1>{item.profile_picture}</h1>
-          <h1>{item.role}</h1>
-        </div>
-      ))}
+    <div className="flex justify-center items-center w-full flex-col lg:gap-y-10">
+      <input
+        placeholder="Search admins..."
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+          setCurrentPage(1);
+        }}
+        className="lg:placeholder:text-sm placeholder:text-xs pl-2 focus:outline-none focus:ring-0"
+      />
+      <Table>
+        <TableHeader>
+          <TableRow className="font-bold">
+            <TableCell>Profile</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Role</TableCell>
+            <TableCell>Joined Date</TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {paginatedData.map((admin, idx) => (
+            <TableRow
+              key={admin.panel_user_id}
+              className={idx % 2 === 0 ? "bg-white" : "bg-neutral-100"}
+            >
+              <TableCell>{admin.profile_picture}</TableCell>
+              <TableCell>{admin.fullname}</TableCell>
+              <TableCell>{admin.email}</TableCell>
+              <TableCell>{admin.role}</TableCell>
+              <TableCell>{admin.created_at.split("T")[0]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      {paginatedData.length !== 0 && (
+        <Pagination className="mt-6">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                className="cursor-pointer"
+                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                // disabled={currentPage === 1}
+              />
+            </PaginationItem>
+            {Array.from({ length: totalPages }).map((_, index) => (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  isActive={currentPage === index + 1}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem>
+              <PaginationNext
+                className="cursor-pointer"
+                onClick={() =>
+                  setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+                }
+                // disabled={currentPage === totalPages}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
     </div>
   );
 };
