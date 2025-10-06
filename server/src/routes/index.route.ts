ff0738802adcae56cbf9e4fa45706827a1d06eb7
Saad Sayyed <saadsyed950@gmail.com>
2025-10-06 16:59:56 +0530

    Panel user API testing

--- server/src/routes/index.route.ts
+++ server/src/routes/index.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import panelUserRouter from "./panel-user.route";
 
 const mainRouter = Router();
+
+panelUserRouter.use("/panel-user");
 
 export default mainRouter;
