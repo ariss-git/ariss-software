4281b82a8eb983630c1b18959d3496abf4b824fd
Saad Sayyed <saadsyed950@gmail.com>
2025-10-06 14:43:04 +0530

    Database setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,15 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mainRouter from "./routes/index.route";
 
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", mainRouter);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Server is working on this URI");
