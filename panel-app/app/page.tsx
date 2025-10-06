191f2bc29fae26ccbc792b0d6f31bc40db375658
Saad Sayyed <saadsyed950@gmail.com>
2025-10-06 17:29:04 +0530

    ShadCN setup

--- panel-app/app/page.tsx
+++ panel-app/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen">
+    <div className="flex justify-center items-center w-full min-h-screen flex-col">
       <h1 className="text-8xl">Home</h1>
+      <Button>Press</Button>
     </div>
   );
 };
