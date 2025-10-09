0b7994f6a7538715cc695ab4987e2098d7ea16ae
Saad Sayyed <saadsyed950@gmail.com>
2025-10-09 14:08:57 +0530

    App cleaned (restarting)

--- panel-app/app/_api/api-url.ts
+++
@@ -1,3 +0,0 @@
-const apiURL = "http://localhost:5000/api";
-
-export default apiURL;
--- server/src/routes/index.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import panelUserRouter from "./panel-user.route";
-
-const mainRouter = Router();
-
-mainRouter.use("/panel-user", panelUserRouter);
-
-export default mainRouter;
