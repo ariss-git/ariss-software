a26d69f58d8bcbe5eb5f7cd37e09e1058311b16d
Saad Sayyed <saadsyed950@gmail.com>
2025-10-09 17:52:23 +0530

    Panel user route

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mainRouter from "./api/routes/index.route.js";
 
 dotenv.config();
 const PORT = 5000;
@@ -9,5 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", mainRouter);
 
 app.listen(PORT, () => console.log("Server running on PORT:", PORT));
