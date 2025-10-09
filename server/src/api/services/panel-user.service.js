7167d737cfd0008bed6860fe1924c7079dea7d66
Saad Sayyed <saadsyed950@gmail.com>
2025-10-09 16:57:12 +0530

    Working on API of Priority Module.

---
+++ server/src/api/services/panel-user.service.js
@@ -0,0 +1,8 @@
+import { v4 as uuid } from "uuid";
+import prisma from "../../lib/orm.js";
+
+export class PanelUserService {
+  async registerPanelUser(clerkId, email, fullname, role) {
+    // TODO: Work on this method
+  }
+}
