e605f767cd477f144f8719e3237d9d126d03b126
Saad Sayyed <saadsyed950@gmail.com>
2025-10-09 17:41:02 +0530

    Panel user service

--- server/src/api/services/panel-user.service.js
+++ server/src/api/services/panel-user.service.js
@@ -1,8 +1,48 @@
 import { v4 as uuid } from "uuid";
 import prisma from "../../lib/orm.js";
+import { PanelUserType } from "@prisma/client";
 
 export class PanelUserService {
   async registerPanelUser(clerkId, email, fullname, role) {
-    // TODO: Work on this method
+    const exisitingUser = await prisma.panelUser.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (exisitingUser)
+      throw new Error("Panel user with this account already exists.");
+
+    const panelUser = await prisma.panelUser.create({
+      data: {
+        id: `${role}-${uuid()}`,
+        clerk_id: clerkId,
+        email,
+        fullname,
+        role,
+      },
+    });
+
+    return panelUser;
+  }
+
+  async fetchAllPanelUsers() {
+    return await prisma.panelUser.findMany();
+  }
+
+  async fetchAllAdmin() {
+    return await prisma.panelUser.findMany({
+      where: {
+        role: PanelUserType.ADMIN,
+      },
+    });
+  }
+
+  async fetchAllEmployee() {
+    return await prisma.panelUser.findMany({
+      where: {
+        role: PanelUserType.EMPLOYEE,
+      },
+    });
   }
 }
