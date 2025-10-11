d727a1f11d608a77123476c953d53f981612ffbc
Saad Sayyed <saadsyed950@gmail.com>
2025-10-11 12:51:53 +0530

    Subcategory API

--- server/src/api/routes/stock/index.route.js
+++ server/src/api/routes/stock/index.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import categoryRouter from "./category.route.js";
+import subcategoryRouter from "./subcategory.route.js";
 
 const stockRouter = Router();
 
 stockRouter.use("/category", categoryRouter);
+stockRouter.use("/subcategory", subcategoryRouter);
 
 export default stockRouter;
