d6ecdabf173da4fea20716bcb73ff7292923130c
Saad Sayyed <saadsyed950@gmail.com>
2025-10-11 11:10:21 +0530

    Category service

---
+++ server/src/api/services/stock/category.service.js
@@ -0,0 +1,95 @@
+import prisma from "../../../lib/orm.js";
+import { createNotification } from "../notification.service.js";
+import { v4 as uuid } from "uuid";
+
+export class CategoryService {
+  async addCategory(title, description, image, panelUserId) {
+    const exisiting = await prisma.category.findUnique({
+      where: {
+        title,
+      },
+    });
+
+    if (exisiting) throw new Error("Category already exists");
+
+    const category = await prisma.category.create({
+      data: {
+        id: `category-${uuid()}`,
+        title,
+        description,
+        image,
+        panel_user_id: panelUserId,
+      },
+    });
+
+    createNotification(
+      "Category added",
+      `New ${category.title} added to the stocks`,
+      category.panel_user_id
+    );
+
+    return panelUserId;
+  }
+
+  async getAllCategory() {
+    return await prisma.category.findMany();
+  }
+
+  async getSingleCategory(id) {
+    const category = await prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!category) throw new Error("Category do not exist");
+
+    return category;
+  }
+
+  async updateCategory(id, title, description, image, panelUserId) {
+    const exisiting = await prisma.category.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!exisiting) throw new Error("Category do not exist");
+
+    const category = await prisma.category.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        image,
+        panel_user_id: panelUserId,
+      },
+    });
+
+    createNotification(
+      "Category updated",
+      `${category.title} has some new changes`,
+      category.panel_user_id
+    );
+
+    return category;
+  }
+
+  async deleteCategory(id, panelUserId) {
+    const category = await prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+
+    createNotification(
+      "Category deleted",
+      `${category.title} has been removed permanently`,
+      panelUserId
+    );
+
+    return category;
+  }
+}
