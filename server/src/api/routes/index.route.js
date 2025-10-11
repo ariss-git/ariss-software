b3d7624522b5be4f52b4179709c4b1a24cc7b9cd
Saad Sayyed <saadsyed950@gmail.com>
2025-10-11 10:55:12 +0530

    Tables added

--- server/src/api/routes/index.route.js
+++ server/src/api/routes/index.route.js
@@ -3,6 +3,7 @@ import panelUserRouter from "./panel-user.route.js";
 import notificationRouter from "./notification.route.js";
 import otpRouter from "./otp.route.js";
 import customerRouter from "./customers/index.route.js";
+import stockRouter from "./stock/index.route.js";
 
 const mainRouter = Router();
 
@@ -10,5 +11,6 @@ mainRouter.use("/panel-user", panelUserRouter);
 mainRouter.use("/notification", notificationRouter);
 mainRouter.use("/otp", otpRouter);
 mainRouter.use("/customer", customerRouter);
+mainRouter.use("/stock", stockRouter);
 
 export default mainRouter;
---
+++ server/src/api/routes/stock/index.route.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const stockRouter = Router();
+
+export default stockRouter;
