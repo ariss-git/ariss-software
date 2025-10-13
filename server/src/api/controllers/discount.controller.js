67560471dfc7ae295ec7f5ab30fde4bc8484d735
Saad Sayyed <saadsyed950@gmail.com>
2025-10-13 17:50:11 +0530

    Discount controller

--- server/src/api/controllers/discount.controller.js
+++ server/src/api/controllers/discount.controller.js
@@ -23,3 +23,44 @@ export const assignDiscountController = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const fetchAllDiscountsController = async (_req, res) => {
+  try {
+    const discount = await discountServices.fetchAllDiscounts();
+    res.status(200).json({ total: discount.length, data: discount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const fetchSingleDiscountController = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(404).json({ message: "ID not found in params" });
+  }
+
+  try {
+    const discount = await discountServices.fetchAllDiscounts();
+    res.status(200).json({ total: discount.length, data: discount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteDiscountController = async (req, res) => {
+  const { id, panelId } = req.params;
+
+  if (!id) {
+    return res
+      .status(404)
+      .json({ message: "ID and Panel ID not found in params" });
+  }
+
+  try {
+    const discount = await discountServices.deleteDiscount(id, panelId);
+    res.status(200).json({ message: "Discount deleted", data: discount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
