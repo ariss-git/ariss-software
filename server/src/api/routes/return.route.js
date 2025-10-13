615bb63b62610e36624610d9db5d67debdbb8040
Saad Sayyed <saadsyed950@gmail.com>
2025-10-13 19:43:20 +0530

    RMA Route

---
+++ server/src/api/routes/return.route.js
@@ -0,0 +1,24 @@
+import * as returnControllers from "../controllers/return.controller.js";
+import { Router } from "express";
+
+const returnRouter = Router();
+
+returnRouter.post("/create", returnControllers.createReturnRequestController);
+
+returnRouter.get("/all", returnControllers.fetchAllReturnsController);
+
+returnRouter.patch(
+  "/approve/:id",
+  returnControllers.approveReturnRequestController
+);
+returnRouter.patch(
+  "/reject/:id",
+  returnControllers.rejectReturnRequestController
+);
+
+returnRouter.delete(
+  "/delete/:id",
+  returnControllers.deleteReturnRequestController
+);
+
+export default returnRouter;
